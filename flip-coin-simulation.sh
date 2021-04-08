6348f783e7ad5a16031428d699b2f9a86fe5f603
iamatharv <iamatharvaj@gmail.com>
2021-04-08 20:18:55 +0530

    Added: Solution for number of

--- flip-coin-simulation.sh
+++ flip-coin-simulation.sh
@@ -7,3 +7,37 @@ function flipCoin(){
 	result=$(( RANDOM%2 ))
 	echo $result
 }
+
+#Use-case-2 Flipping a coin and storing it in a dictionary
+isHead=0
+isTail=1
+count=10
+headCount=0
+tailCount=0
+
+declare -A singlet
+
+for ((  i=0 ; i<count ; i++ ))
+do
+	singlet[$i]=$( flipCoin )
+	if [[ singlet[$i] -eq isHead ]]
+	then
+		(( headCount++ ))
+	elif [[ singlet[$i] -eq isTail ]]
+	then
+		(( tailCount++ ))
+	fi
+done
+echo ${singlet[@]}
+
+#Function for calculating percentage
+function percentage(){
+	count=`echo "$1*0.1"|bc`
+	percent=`echo "$count*100"|bc`
+	echo $percent
+}
+
+singletHeadPercentage=$( percentage $headCount )
+echo $singletHeadPercentage
+singletTailPercentage=$( percentage $tailCount )
+echo $singletTailPercentage
