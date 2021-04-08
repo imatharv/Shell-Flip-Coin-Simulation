50519bcc093006eb16221005020deedc085a13e5
iamatharv <iamatharvaj@gmail.com>
2021-04-08 20:46:53 +0530

    Added: Solution to show if it’s a Win or Tie. If Win then who won and by how much

--- flip-coin-simulation.sh
+++ flip-coin-simulation.sh
@@ -25,5 +25,37 @@ do
 	fi
 done
 
+#Use-case-3 Show if it’s a Win or Tie. If Win then who won and by how much
+if [[ $headWinCounter -eq $tailWinCounter ]]
+then
+	echo "We have a tie. Rolling again..."
+
+	while [[ $(($headWinCounter-$tailWinCounter)) -lt 2 && $(($tailWinCounter-$headWinCounter)) -lt 2  ]]
+	do
+   		flip=$((RANDOM%2))
+
+   		if [[ $flip -eq 1 ]]
+   		then
+      			((headWinCounter++))
+   		else
+      			((tailWinCounter++))
+   		fi
+	done
+
+	if [[ $headWinCounter -gt $tailWinCounter ]]
+	then
+   		echo "HEADS has won by $((headWinCounter-tailWinCounter)) tosses"
+	else
+   		echo "TAILS has won by $((tailWinCounter-headWinCounter)) tosses"
+	fi
+
+	elif [[ $headWinCounter -gt $tailWinCounter ]]
+	then
+		echo "HEADS has won by $((headWinCounter-tailWinCounter)) tosses"
+	else
+		echo "TAILS has won by $((tailWinCounter-headWinCounter)) tosses"
+	fi
+fi
+
 echo "Number of times HEAD won: $headWinCounter"
 echo "Number of times TAIL won: $tailWinCounter"
